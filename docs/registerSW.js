2b9b40aedc33c89f4ca2c8fd3fbe3d3e7d8c6d76
patdelphi <patdelphi@outlook.com>
2025-07-17 10:01:59 +0800

    fix: 修复 GitHub Pages 部署路径问题

--- docs/registerSW.js
+++ docs/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/pdx-json-editor/sw.js', { scope: '/pdx-json-editor/' })})}
